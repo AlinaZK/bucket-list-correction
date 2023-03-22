6ff6c975322186c7054ef52961625f33797f49f6
zpineau2022 <zkalagova@yahoo.com>
2023-03-22 17:35:04 +0100

    zalina

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\SerieType;
+use App\Form\WishType;
 use App\Repository\WishRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 #[Route('/wish/', name: 'wish_')]
 class WishController extends AbstractController
@@ -32,6 +36,46 @@ class WishController extends AbstractController
         return $this->render('wish/detail.html.twig', [
             'wish' => $wish
         ]);
+    }
+    #[Route('chose', name: 'chose')]
+    public function chose(Request $request,EntityManagerInterface $entityManager): Response{
+
+        $chose = new Wish();
+
+        $choseForm = $this->createForm (WishType::class, $chose);
+
+        dump($chose);// pour affiche la defirance
+
+        // hydrade l'instance $chose avec donnes de la request
+        $choseForm->handleRequest($request);
+        dump($chose);
+
+
+        // test si le formulaire a ete soumis
+        if($choseForm->isSubmitted() && $choseForm->isValid()){
+
+            $chose->setDateCreated(new \DateTime());
+            $chose->setIsPublished(true);
+
+            // entresitre le souit en BDD
+            $entityManager->persist($chose);
+            $entityManager->flush();
+
+// messge flash odnamomentnie odnorazovie
+            $this->addFlash('success', 'Idea successfully added!');//  attention bien mettre (type) !!!!
+
+            return $this ->redirectToRoute('wish_detail', ['id'=> $chose->getId()]);
+        }
+
+        dump($request);
+
+
+        return $this ->render('/wish/chose.html.twig', [
+
+            'choseForm'=>$choseForm
+        ]);
+
+
     }
 
 
--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\WishRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: WishRepository::class)]
 class Wish
@@ -14,10 +15,15 @@ class Wish
     #[ORM\Column]
     private ?int $id = null;
 
+
+    // the two lines first of all to check if it's not empty
+    // second to make a limits sizing
+    #[Assert\NotBlank(message: 'Please provide a title')]
+    #[Assert\Length(min: 2, max: 250, minMessage: 'Your title is to short',maxMessage: 'Your title is to long ',)]
     #[ORM\Column(length: 250)]
     private ?string $title = null;
 
-
+    #[Assert\Length(min: 0, max: 50, minMessage: 'Your author is to short characters long',maxMessage: 'Your author is to long characters',)]
     #[ORM\Column(length: 50)]
     private ?string $author = null;
 
@@ -27,8 +33,13 @@ class Wish
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $dateCreated = null;
 
+    #[Assert\NotBlank(message: 'a remplir.')]
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'wish')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
 
     public function getId(): ?int
     {
@@ -91,6 +102,18 @@ class Wish
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
